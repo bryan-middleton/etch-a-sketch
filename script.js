6ffb82dd21d7aa99b2b4b4657486349fb51f7390
Bryan Middleton <bryanmiddleton1986@gmail.com>
2022-11-24 15:08:46 +0000

    grid created, drawing works

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Doctype javascript
 
-let k=0;//for debugging
-//loop to create 16x16 divs
-for(let i = 0; i<16; i++){
-    k++;
-    for(let j = 0; j<16; j++){
-        const div = document.createElement('div');
-        div.style.border ='1px solid blue';
-        //div.style.display ='flex';
-        div.style.height = '200px';
-        div.style.width = '200px';
-        div.textContent = 'test '+k;
-        document.getElementById('mainContainer').appendChild(div);
-        k++
-    }
+
+//create 256 divs and add to container
+for(let i=1;i<257;i++){
+    const div = document.createElement('div');
+    div.classList.add('gridItem');
+    div.textContent = i; //debugging
+    document.getElementById('gridContainer').appendChild(div);
 }
+
+//add class when mouse hovers over box
+const boxes = document.querySelectorAll('.gridItem');
+boxes.forEach((box) => {
+    box.addEventListener('mouseover',()=>{
+        box.classList.add('drawn') });
+    });
